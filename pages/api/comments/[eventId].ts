6e28f6b329d42f09d367265d9291b184804882c1
Saddam Mukti <saddam.coder@gmail.com>
2023-12-10 00:01:20 +0700

    use mongodb

--- pages/api/comments/[eventId].ts
+++ pages/api/comments/[eventId].ts
@@ -1,12 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { CommentData } from "@/components/input/new-comment";
+import { MongoClient } from "mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { MONGO_URL } from "@/helpers/mongo-utils";
 
 type Response = {
   message: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Response | CommentData[] | string>,
 ) {
@@ -26,17 +28,32 @@ export default function handler(
       return res.status(422).json({ message: "Invalid input" });
     }
 
+    const client = await MongoClient.connect(MONGO_URL);
+    const db = client.db();
+    await db.collection("comments").insertOne({ eventId, email, name, text });
+    await client.close();
+
     res.status(200).json({ message: "Comment saved!" });
   }
 
   if (req.method === "GET") {
-    return res.status(200).json([
-      {
-        name: "Saddam Azy",
-        email: "myxzlpltk@gmail.com",
-        text: "Lorem ipsum dolor sit amet",
-      },
-    ]);
+    const client = await MongoClient.connect(MONGO_URL);
+    const db = client.db();
+    const docs = await db
+      .collection("comments")
+      .find({ eventId })
+      .sort({ _id: -1 })
+      .toArray()
+      .then((data) =>
+        data.map((e) => ({
+          name: e.name,
+          email: e.email,
+          text: e.text,
+        })),
+      );
+    await client.close();
+
+    return res.status(200).json(docs);
   }
 
   res.status(405).send("Method Not Allowed");
--- pages/api/newsletters.ts
+++ pages/api/newsletters.ts
@@ -1,11 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient } from "mongodb";
+import { MONGO_URL } from "@/helpers/mongo-utils";
 
 type Response = {
   message: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Response | string>,
 ) {
@@ -14,6 +16,11 @@ export default function handler(
     if (!email || !email.includes("@")) {
       return res.status(422).json({ message: "Invalid email address" });
     }
+
+    const client = await MongoClient.connect(MONGO_URL);
+    const db = client.db();
+    await db.collection("newsletters").insertOne({ email });
+    await client.close();
 
     return res.status(200).json({ message: "Signed Up!" });
   }
